df3f7ad803eece4e5a90b1572df8f69832064720
Jefflie <sonicscrewdriver79@gmail.com>
2020-12-09 02:59:07 -0500

    Update curvatures_at_point.h

--- include/curvatures_at_point.h
+++ include/curvatures_at_point.h
@@ -3,17 +3,20 @@
 #include <Eigen/Core>
 
 /*
-This function computes the principal curvature and principal curvature directions at a specific vertex, indexed by v
+This function computes the corrected principal curvature and corrected principal curvature directions at vertex v
 
 Inputs:
-    K int chosen significantly greater than maximal curvature. Set to 1000, for example.
-    V #V by 3 list of mesh vertex positions
-    F #F by 3 list of mesh face indices into V
+    K     int chosen significantly greater than maximal curvature. Set to 1000 by default in main.cpp
+    r     Scale parameter (radius). Set to 0.001 by default in main.cpp
+    V     #V by 3 list of mesh vertex positions
+    F     #F by 3 list of mesh face vertex indices
 Outputs:
-    D1 #V by 3 list of corrected first principal curvature directions
-    D2 #V by 3 list of corrected second principal curvature directions
-    K1 #V by 1 list of corrected first principal curvatures
-    K2 #V by 1 list of corrected second principal curvatures
+    D1  #V by 3 list of corrected first principal curvature directions
+    D2  #V by 3 list of corrected second principal curvature directions
+    K1  #V by 1 list of corrected first principal curvatures
+    K2  #V by 1 list of corrected second principal curvatures
+    G   #V by 1 list of corrected Gaussian curvatures
+    H   #V by 1 list of corrected mean curvatures
 */
 
 
